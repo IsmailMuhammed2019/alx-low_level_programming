05c02beee0ea418b0290e4fc1fb445730ac5141d
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-09-21 08:41:24 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,41 +1,51 @@
 #include "main.h"
 
 /**
- * times_table - prints the times table up to 9
+ * print_times_table - prints the times table up to the integer input
+ * @n: integer input
  *
  * Return: void
  */
 
-void times_table(void)
+void print_times_table(int n)
 {
-	int a, b, c, d, e;
+	int a, b, c, d, e, f;
 
-	for (a = 0; a < 10; a++)
+	if (n >= 0 && n < 15)
 	{
-		for (b = 0; b < 10; b++)
+		for (a = 0; a <=  n; a++)
 		{
-			c = a * b;
-			d = c / 10;
-			e = c % 10;
-			if (b == 0)
-				_putchar('0');
-			else if (c < 10)
+			for (b = 0; b <= n; b++)
 			{
-				_putchar(' ');
-				_putchar('0' + e);
+				c = a * b; d = c / 100; e = (c /  10) % 10;
+				f = (c % 100) % 10;
+				if (b == 0)
+				{
+					_putchar('0');
+				}
+				else if (c < 10)
+				{
+					_putchar(' '); _putchar(' ');
+					_putchar('0' + f);
+				}
+				else if (c < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + e); _putchar('0' + f);
+				}
+				else
+				{
+					_putchar('0' + d);
+					_putchar('0' + e);
+					_putchar('0' + f);
+				}
+				if (b < n)
+				{
+					_putchar(','); _putchar(' ');
+				}
+				else
+					_putchar('\n');
 			}
-			else
-			{
-				_putchar('0' + d);
-				_putchar('0' + e);
-			}
-			if (b < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-				_putchar('\n');
 		}
 	}
 }
