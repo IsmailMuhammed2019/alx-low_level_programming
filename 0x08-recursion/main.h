9ca6596b6c074bdd6a930ee63698a66c3bd93df2
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-10-03 01:29:07 -0700

    Task 1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,6 @@
-int _putchar(char);
+#ifndef MAIN_H
+#define MAIN_H
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -7,5 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-char *wildcard(char *s3, char *s4);
+int wildcmp(char *s1, char *s2); 
+int _putchar(char c);
+
+#endif /*MAIN_H*/
